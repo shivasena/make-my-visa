3f023cb5bd0a5e4dd5a03d60ca51bf42c87740ed
shivasena <mika.smith@gmail.com>
2016-01-09 19:33:50 +0200

    mergefix

--- public/js/client.js
+++ public/js/client.js
@@ -7,19 +7,6 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 
-$(document).ready(function() {
- 
-  $("#owl-flags").owlCarousel({
-      'slideSpeed' : 300,
-      'paginationSpeed' : 400,
-      'loop': true,
-      'lazyLoad': true,
-      'autoplay': true,
-      'items': 2
-  });
- 
-});
-
 $('#orderModal').on('shown.bs.modal', function () {
   $('#phoneInput').focus()
 })
